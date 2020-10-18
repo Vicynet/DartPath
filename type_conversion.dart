d7b1e3a989ef4c5cc297d26ef9652b93149076d1
Victor Ihedioha <ihedioha.victor@gmail.com>
2020-10-18 18:16:01 +0100

    Type conversion

--- type_conversion.dart
+++ type_conversion.dart
@@ -4,4 +4,10 @@ main(List<String> args) {
 
   var doubleOne = double.parse('1.1');
   assert(doubleOne==1.1);
+
+  String oneAsString = 1.toString();
+  assert(oneAsString=='1');
+
+  String piAsString = 3.14159.toStringAsFixed(2);
+  assert(piAsString=='3.14');
 }
